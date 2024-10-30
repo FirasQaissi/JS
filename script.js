0f0596e959b361e17573136881e239686975d874
FirasQaissi <58230285+FirasQaissi@users.noreply.github.com>
2024-10-30 19:54:58 +0200

    Script

---
+++ script.js
@@ -0,0 +1,16 @@
+var total = 100
+console.log(total);
+console.log(total == 100);
+
+var itOk = 10;
+console.log(itOk == 10);
+console.log(itOk !== 10);
+itOk++;
+itOk = itOk + 1;
+itOk += 1;
+
+
+!!
+
+
+console.log(itOk);
