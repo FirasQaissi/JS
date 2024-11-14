a8e548155e600de8b7e9a48b6a57ed235ce4f1ac
FirasQaissi <58230285+FirasQaissi@users.noreply.github.com>
2024-11-15 00:28:04 +0200

    ASA

--- 14.11.24/script5.js
+++ 14.11.24/script5.js
@@ -1,31 +1,59 @@
 
-console.log('************for of *********');
-let flags = ['israel', 'england', 'france', 'spain', 'india', 'USA', 'China'];
+//console.log('************for of *********');
+//let flags = ['israel', 'england', 'france', 'spain', 'india', 'USA', 'China'];
 
-for (let flag of flags) {
-    console.log('flag is ' + flag);
+//for (let flag of flags) {
+//  console.log('flag is ' + flag);
+
+//}
+//console.log('************for in *********');
+
+//for (let i in flags) {
+//  console.log('the flag is :' + flags[6]);
+
+//}
+
+//console.log('************Break - contunie *********');
+
+//const numbers = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000];
+//for (let i in numbers) {
+//  if (i == 2) {
+//    break;
+//}
+//console.log('number is ' + numbers[i]);
+//if (numbers[i] === 500) {
+//   continue;
+// }
+
+//console.log(numbers[i]);
+
+//
+//}
+
+let actors = ['jordan', 'kobe', 'wade', 'nash', 'harden'];
+
+for (let actor of actors) {
+    const item = actor.split('')
+    item[0] = item[0].toUpperCase();
+    let newtext = item.join('');
+    console.log(newtext);
 
 }
-console.log('************for in *********');
 
-for (let i in flags) {
-    console.log('the flag is :' + flags[6]);
+'************Filter *********'
 
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const oddNumber = numbers.filter(function (item) {
+    return item % 2 == 0;
 }
 
-console.log('************Break - contunie *********');
+)
 
-const numbers = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000];
-for (let i in numbers) {
-    if (i == 2) {
-        break;
-    }
-    console.log('number is ' + numbers[i]);
-    if (numbers[i] === 500) {
-        continue;
-    }
-
-    console.log(numbers[i]);
-
-
+for (let i in oddNumber) {
+    oddNumber[i] *= 100;
 }
+console.log(oddNumber);
+
+
+
