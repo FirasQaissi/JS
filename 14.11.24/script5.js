e64768c57ec5e5125683fc56e39fc4029234368c
FirasQaissi <58230285+FirasQaissi@users.noreply.github.com>
2024-11-14 19:08:07 +0200

    A

--- 14.11.24/script5.js
+++ 14.11.24/script5.js
@@ -9,6 +9,23 @@ for (let flag of flags) {
 console.log('************for in *********');
 
 for (let i in flags) {
-    console.log('the flag is :' + flags[i]);
+    console.log('the flag is :' + flags[6]);
+
+}
+
+console.log('************Break - contunie *********');
+
+const numbers = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000];
+for (let i in numbers) {
+    if (i == 2) {
+        break;
+    }
+    console.log('number is ' + numbers[i]);
+    if (numbers[i] === 500) {
+        continue;
+    }
+
+    console.log(numbers[i]);
+
 
 }
