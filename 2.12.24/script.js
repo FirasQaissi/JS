5ba9f667abfb22fc34e4110a7ea51149282d5d34
FirasQaissi <58230285+FirasQaissi@users.noreply.github.com>
2024-12-04 08:25:53 +0200

    objects

--- 2.12.24/script.js
+++ 2.12.24/script.js
@@ -55,3 +55,71 @@ for (let obj of Object.values(car)) {
 
 console.log(Object.keys(car));
 console.log(Object.values(car));
+
+
+
+let user = {
+    first: 'firas',
+    last: 'Qaissi',
+    age: 33,
+    weight: 75,
+    location: 'jerusalem',
+    color: 'blue',
+    password: '123456',
+    isLogin: true,
+    oldPass: 1234,
+
+
+
+    changeFullName: function (first, last) {
+        this.first = first;
+        this.last = last;
+    },
+
+
+    login: function () {
+        let username = prompt('UserName Please?');
+        let password1 = +prompt('Your Password?');
+        if (username === user.first || password1 === user.password) {
+            this.isLogin = true, alert('sucsses')
+        } else { alert('try again') }
+
+    },
+
+    logout: function () {
+        let lgout = prompt('are you want to logout?')
+        if (lgout === 'yes') {
+            this.isLogin = false;
+        }
+    },
+
+    changePassword: function () {
+        let firstpass = +prompt('Enter Your First Password?');
+
+        if (firstpass === this.password) {
+            prompt('enter your newPass Please');
+
+        } else { alert('not good') }
+    },
+
+}
+
+
+
+console.log(user);
+console.log(typeof user);
+
+function say() {
+    console.log('hi Helloo ' + user.first + ' ' + user.last);
+
+}
+say(user)
+console.log(user);
+
+user.changeFullName('david', 'jimmy')
+console.log(`${user.first} ${user.last}`);
+
+
+
+user.changePassword()
+
